e7bf5985bbca75aff13ddf5e65e72e4a1d076fd8
Ravage <2299952@uane.mx>
2024-02-12 22:03:46 -0600

    Remove trailing slash from site URL

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,5 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()] ,
-  site: 'https://dxravage.github.io',
-  base: '/dxravage.github.io/'
+  site: 'https://dxravage.github.io'
 });
