b5752b9f65dad0484a46e54cf61eac62eb7510f5
Ravage <2299952@uane.mx>
2024-02-12 21:58:26 -0600

    Add base URL to astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,6 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()] ,
-  site: 'https://dxravage.github.io'
+  site: 'https://dxravage.github.io',
+  base: '/dxravage.github.io/'
 });
