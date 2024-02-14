dc6bf35d41f26624a3b499b7c7437a7fe9c8a3de
Ravage <2299952@uane.mx>
2024-02-14 16:17:18 -0600

    Update package.json and tailwind.config.mjs, delete unused image and components, add new Table component

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -9,3 +9,15 @@ export default {
 	},
 	plugins: [],
 }
+module.exports = {
+	content: [
+		'./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}',
+		'./node_modules/flowbite/**/*.js'
+	],
+	theme: {
+		extend: {},
+	},
+	plugins: [
+		require('flowbite/plugin')
+	],
+}
