075d0c90e2f3501ea70f17578a183f5dc65aeab7
dxravage <marianoavilaj4@gmail.com>
2025-01-22 18:22:08 +0000

    Add base path configuration to astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,5 +7,6 @@ import lottie from "astro-integration-lottie";
 export default defineConfig({
   integrations: [tailwind(), lottie()],    
   
-  site: 'https://dxravage.github.io'
+  site: 'https://dxravage.github.io',
+  base: '/dxravage.github.io'
 });
