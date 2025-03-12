8160a6fc9db976ed584209d8dac4be30cc05374f
Ravage <112978864+DxRavage@users.noreply.github.com>
2025-03-12 12:13:06 -0600

    Remove base path configuration from Astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,8 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import lottie from "astro-integration-lottie";
 
-// https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), lottie()],    
   
   site: 'https://dxravage.github.io',
-  base: '/dxravage.github.io'
 });
