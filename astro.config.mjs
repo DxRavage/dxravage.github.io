4bbfd705433cc6ecab1cd3a42541af23095fd449
Ravage <112978864+DxRavage@users.noreply.github.com>
2025-03-30 16:27:27 -0600

    Set base URL in Astro config for deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,4 +6,5 @@ import lottie from "astro-integration-lottie";
 export default defineConfig({
   integrations: [tailwind(), lottie()],    
   site: 'https://dxravage.github.io',
+  base: 'dxravage.github.io',
 });
