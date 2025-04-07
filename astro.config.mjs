3b0acf6b984f94be1c7eded7bb7844e8567545c0
Ravage <112978864+DxRavage@users.noreply.github.com>
2025-04-07 13:35:31 -0600

    Add logo assets and update paths in Details component for consistency

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,21 @@
-import { defineConfig } from "astro/config";
-
-import tailwind from "@astrojs/tailwind";
+import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
 import lottie from "astro-integration-lottie";
 
 export default defineConfig({
   integrations: [tailwind(), lottie()],
-  site: "https://dxravage.github.io",
+  site: 'https://dxravage.github.io',
+  vite: {
+    resolve: {
+      alias: {
+        '@images': new URL('./src/images', import.meta.url).pathname,
+        '@components': new URL('./src/components', import.meta.url).pathname,
+      }
+    }
+  },
+  experimental: {
+    svg: {
+      mode: 'sprite',
+    }
+  }
 });
