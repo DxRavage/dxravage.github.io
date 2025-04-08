3b360a4575d22e73cd271b81e3537310c87f7c43
Ravage <112978864+DxRavage@users.noreply.github.com>
2025-04-08 17:43:14 -0600

    Add file URL resolution for image and component aliases in config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import lottie from "astro-integration-lottie";
+import { fileURLToPath, URL } from 'node:url';
 
 export default defineConfig({
   integrations: [tailwind(), lottie()],
@@ -9,5 +10,11 @@ export default defineConfig({
     svg: {
       mode: 'sprite',
     }
-  }
+  },
+  resolve: {
+    alias: {
+      '@images': fileURLToPath(new URL('./src/images', import.meta.url)),
+      '@components': fileURLToPath(new URL('./src/components', import.meta.url)),
+    },
+  },
 });
