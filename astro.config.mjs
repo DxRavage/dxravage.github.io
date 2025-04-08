7ba9564d796fef43b37be32251bf7b3c52c9aa3d
Ravage <112978864+DxRavage@users.noreply.github.com>
2025-04-08 17:02:50 -0600

    Refactor image path and update on components

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,14 +5,6 @@ import lottie from "astro-integration-lottie";
 export default defineConfig({
   integrations: [tailwind(), lottie()],
   site: 'https://dxravage.github.io',
-  vite: {
-    resolve: {
-      alias: {
-        '@images': new URL('./src/images', import.meta.url).pathname,
-        '@components': new URL('./src/components', import.meta.url).pathname,
-      }
-    }
-  },
   experimental: {
     svg: {
       mode: 'sprite',
