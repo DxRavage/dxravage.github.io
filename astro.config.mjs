3c595236157b16757b6e7b90bcb65169c5750daf
Ravage <112978864+DxRavage@users.noreply.github.com>
2025-04-08 17:54:13 -0600

    Update Astro configuration and dependencies for improved compatibility

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,20 +1,33 @@
-import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
+import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
 import lottie from "astro-integration-lottie";
-import { fileURLToPath, URL } from 'node:url';
+import { fileURLToPath, URL } from "node:url";
+
+import node from "@astrojs/node";
 
 export default defineConfig({
   integrations: [tailwind(), lottie()],
-  site: 'https://dxravage.github.io',
+  site: "https://dxravage.github.io",
+  output: "server",
+  adapter: node({
+    mode: "middleware",
+  }),
   experimental: {
     svg: {
-      mode: 'sprite',
-    }
-  },
-  resolve: {
-    alias: {
-      '@images': fileURLToPath(new URL('./src/images', import.meta.url)),
-      '@components': fileURLToPath(new URL('./src/components', import.meta.url)),
+      mode: "sprite",
     },
   },
+
+  resolve: {
+    alias: {
+      "@images": fileURLToPath(new URL("./src/images", import.meta.url)),
+      "@components": fileURLToPath(
+        new URL("./src/components", import.meta.url)
+      ),
+    },
+  },
+
+  adapter: node({
+    mode: "standalone",
+  }),
 });
