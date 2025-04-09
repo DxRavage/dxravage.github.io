fe9a73c135e60c26f181397014475f4ffd0f5fac
Ravage <112978864+DxRavage@users.noreply.github.com>
2025-04-08 22:15:00 -0600

    Refactor image assets: replace PNG files with WEBP format for improved performance and reduced file size. Update component imports and project pages to reflect new asset paths. Clean up unused image imports in various components and layouts.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,33 +1,9 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import lottie from "astro-integration-lottie";
-import { fileURLToPath, URL } from "node:url";
-
-import node from "@astrojs/node";
 
 export default defineConfig({
-  integrations: [tailwind(), lottie()],
+  integrations: [tailwind(), lottie(), ],
   site: "https://dxravage.github.io",
-  output: "server",
-  adapter: node({
-    mode: "middleware",
-  }),
-  experimental: {
-    svg: {
-      mode: "sprite",
-    },
-  },
 
-  resolve: {
-    alias: {
-      "@images": fileURLToPath(new URL("./src/images", import.meta.url)),
-      "@components": fileURLToPath(
-        new URL("./src/components", import.meta.url)
-      ),
-    },
-  },
-
-  adapter: node({
-    mode: "standalone",
-  }),
 });
